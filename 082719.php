d14fc2993e03507dcced0ed0467f8aaf6bf10b8e
Michael Roth <michael.franklin.roth@gmail.com>
2019-08-28 22:38:59 -0400

    more cw

--- 082719.php
+++ 082719.php
@@ -14,4 +14,35 @@ function sumArray($array) {
       }
     }
 }
+
+
+// mens to boys https://www.codewars.com/kata/5af15a37de4c7f223e00012d/
+function menFromBoys($arr) {
+  $evens = array();
+  $odds = array();
+  foreach($arr as $int){
+    if($int % 2 == 0 && !in_array($int,$evens)){
+      array_push($evens, $int);
+    } elseif($int % 2 != 0 && !in_array($int,$odds)) {
+      array_push($odds, $int);
+    }
+  }
+  sort($evens);
+  rsort($odds);
+  return array_merge($evens,$odds);
+}
+
+//best practice
+function menFromBoys($arr) {
+  $arrs=array_unique($arr);
+    foreach($arrs as $arry){
+    if($arry%2 ==0){$even[]=$arry;}else{$odd[]=$arry;};}
+    sort($even);rsort($odd);
+   $end=array_merge($even,$odd);
+    return $end;
+  }
+
+
+
+  
 ?>
