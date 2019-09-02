0f0df4d35adfb6823336787f8551d46aaa8a0853
Michael Roth <michael.franklin.roth@gmail.com>
2019-09-01 21:07:03 -0400

    update

--- cw090119.js
+++ cw090119.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/are-they-the-same/train/javascript
+
 function comp(array1, array2) {
   if (array1.length == 0 || array2.length == 0) {
     return false;
@@ -25,4 +27,12 @@ function comp(array1, array2) {
     }
     return true;
   }
+}
+
+// best practice 
+
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
 }
